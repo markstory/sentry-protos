8702c5c42b49ffe65f56ef2e5e6b0fc6602c2f71
Zachary Collins <zachary.collins@sentry.io>
2024-07-13 17:04:49 -0700

    Improvements

--- src/generators/js2proto.py
+++ src/generators/js2proto.py
@@ -1,7 +1,8 @@
 import dataclasses
-import glob
 import json
 import os.path
+import re
+import sys
 from os import path
 from typing import Iterator, Any, TypeVar
 
@@ -19,16 +20,19 @@ jinja_env = Environment(
     lstrip_blocks=True,
     line_statement_prefix="#",
 )
-jinja_env.add_extension('jinja2.ext.do')
-jinja_env.globals['len'] = len
+jinja_env.add_extension("jinja2.ext.do")
+jinja_env.globals["len"] = len
+
 
 def register_filter(c: _T) -> _T:
     jinja_env.filters[c.__name__] = c
     return c
 
+
 def register_global(c: _T) -> _T:
     jinja_env.globals[c.__name__] = c
     return c
+
 
 def register_test(c: _T) -> _T:
     jinja_env.tests[c.__name__] = c
@@ -44,35 +48,36 @@ def cleaneup_jsonschema(d: Any) -> dict:
     if isinstance(d, bool):
         d = {}
 
-    if '$ref' in d:
+    if "$ref" in d:
         return d
 
-    if 'definitions' in d:
-        for k, v in d['definitions'].items():
-            d['definitions'][k] = cleaneup_jsonschema(v)
+    if "definitions" in d:
+        for k, v in d["definitions"].items():
+            d["definitions"][k] = cleaneup_jsonschema(v)
 
-    if 'type' in d and isinstance(d['type'], list):
-        if len(d['type']) == 1:
-            d['type'] = d['type'][0]
+    if "type" in d and isinstance(d["type"], list):
+        if len(d["type"]) == 1:
+            d["type"] = d["type"][0]
 
-    if 'anyOf' in d:
-        d['anyOf'] = [cleaneup_jsonschema(v) for v in d['anyOf']]
+    if "anyOf" in d:
+        d["anyOf"] = [cleaneup_jsonschema(v) for v in d["anyOf"]]
 
-    if 'items' in d:
-        if isinstance(d['items'], list):
-            d['prefixItems'] = [cleaneup_jsonschema(v) for v in d['items']]
-            del d['items']
+    if "items" in d:
+        if isinstance(d["items"], list):
+            d["prefixItems"] = [cleaneup_jsonschema(v) for v in d["items"]]
+            del d["items"]
         else:
-            d['items'] = cleaneup_jsonschema(d['items'])
+            d["items"] = cleaneup_jsonschema(d["items"])
 
-    if 'properties' in d:
-        for k, v in d['properties'].items():
-            d['properties'][k] = cleaneup_jsonschema(v)
+    if "properties" in d:
+        for k, v in d["properties"].items():
+            d["properties"][k] = cleaneup_jsonschema(v)
 
-    if 'additionalProperties' in d and isinstance(d['additionalProperties'], dict):
-        d['additionalProperties'] = cleaneup_jsonschema(d['additionalProperties'])
+    if "additionalProperties" in d and isinstance(d["additionalProperties"], dict):
+        d["additionalProperties"] = cleaneup_jsonschema(d["additionalProperties"])
 
     return d
+
 
 @register_global
 @dataclasses.dataclass
@@ -81,17 +86,25 @@ class NamedStructureType:
     properties: "dict[str, NamedSchema]"
     definitions: dict[str, Schema] = dataclasses.field(repr=False)
 
+
 @register_global
 @dataclasses.dataclass
 class ScalarType:
     data_type: DataType | None = None
     format: str | None = None
     well_known_type: str | None = None
+    extension: str | None = None
+
+    @property
+    def extension_text(self) -> str:
+        if self.extension is None:
+            return ""
+        return f"[{self.extension}]"
 
     @property
     def proto_scalar_type(self) -> str:
         if self.format:
-            if self.format in {'uint64', 'uint32', 'int32', 'int64', 'double'}:
+            if self.format in {"uint64", "uint32", "int32", "int64", "double"}:
                 return self.format
             elif self.format in {"uuid"}:
                 return "string"
@@ -111,19 +124,23 @@ class ScalarType:
 
         raise RuntimeError(f"Unsupported type: {self!r}")
 
+
 _any = ScalarType(well_known_type="google.protobuf.Value")
 _struct = ScalarType(well_known_type="google.protobuf.Struct")
 _list = ScalarType(well_known_type="google.protobuf.ListValue")
+
 
 @register_global
 @dataclasses.dataclass
 class MapType:
     inner: ScalarType | NamedStructureType
 
+
 @register_global
 @dataclasses.dataclass
 class RepeatedType:
     inner: ScalarType | NamedStructureType
+
 
 @register_global
 @dataclasses.dataclass
@@ -131,16 +148,19 @@ class NullableType:
     inner: ScalarType | NamedStructureType
     definitions: dict[str, Schema] = dataclasses.field(repr=False)
 
+
 @register_global
 @dataclasses.dataclass
 class SumType:
     parts: "list[NamedSchema]"
+
 
 @dataclasses.dataclass
 class NamedSchema:
     """
     Wrapper that gets injected into the jinja2 template, provides utilities that operate on the schema.
     """
+
     name: str
     schema: Schema
     definitions: dict[str, Schema]
@@ -157,9 +177,22 @@ class NamedSchema:
             return self.definitions[type_name]
         return schema_or_ref
 
-    def unpack(self) -> NamedStructureType | ScalarType | NullableType | RepeatedType | MapType | SumType:
+    def unpack(
+        self,
+    ) -> (
+        NamedStructureType
+        | ScalarType
+        | NullableType
+        | RepeatedType
+        | MapType
+        | SumType
+    ):
         inner_nullable: Schema | None = None
-        if self.schema.anyOf is not None and len(self.schema.anyOf) == 2 and self.schema.anyOf[1].type == DataType.NULL:
+        if (
+            self.schema.anyOf is not None
+            and len(self.schema.anyOf) == 2
+            and self.schema.anyOf[1].type == DataType.NULL
+        ):
             inner_nullable = self.resolve(self.schema.anyOf[0])
 
         if isinstance(self.schema.type, list):
@@ -179,14 +212,22 @@ class NamedSchema:
             return NullableType(inner=unpacked, definitions=self.definitions)
 
         if self.schema.anyOf and len(self.schema.anyOf) == 1:
-            return dataclasses.replace(self, schema=self.resolve(self.schema.anyOf[0])).unpack()
+            return dataclasses.replace(
+                self, schema=self.resolve(self.schema.anyOf[0])
+            ).unpack()
 
         if self.schema.anyOf:
             parts: list[NamedSchema] = []
             for part in self.schema.anyOf:
                 if isinstance(part, Reference):
-                    name = part.ref.split("/")[-1].lower()
-                    parts.append(NamedSchema(name=name, schema=self.resolve(part), definitions=self.definitions))
+                    name = part.ref.split("/")[-1]
+                    parts.append(
+                        NamedSchema(
+                            name=name,
+                            schema=self.resolve(part),
+                            definitions=self.definitions,
+                        )
+                    )
                 else:
                     return _any
             return SumType(parts)
@@ -194,7 +235,27 @@ class NamedSchema:
         inner_repeated: Schema | None = None
         if self.schema.type is not None and self.schema.type == DataType.ARRAY:
             if self.schema.prefixItems:
-                return _list
+                # doesn't support mixing the two
+                if self.schema.items:
+                    return _list
+                result = NamedStructureType(
+                    definition_dependencies=[],
+                    properties={},
+                    definitions=self.definitions,
+                )
+                for i, item in enumerate(self.schema.prefixItems):
+                    named = result.properties[f"a{i}"] = NamedSchema(
+                        name="",
+                        schema=self.resolve(item),
+                        definitions=self.definitions,
+                    )
+                    if isinstance(item, Reference):
+                        named.name = item.ref.split("/")[-1]
+                    else:
+                        named.name = f"{self.name}{i}"
+                        result.definition_dependencies.append(f"a{i}")
+                return result
+
             if not self.schema.items:
                 raise RuntimeError(f"Unsupported data type: {self.schema}")
             inner_repeated = self.resolve(self.schema.items)
@@ -209,7 +270,11 @@ class NamedSchema:
             return RepeatedType(unpacked)
 
         inner_map: Schema | None = None
-        if self.schema.type == DataType.OBJECT and not self.schema.properties and isinstance(self.schema.additionalProperties, (Reference, Schema)):
+        if (
+            self.schema.type == DataType.OBJECT
+            and not self.schema.properties
+            and isinstance(self.schema.additionalProperties, (Reference, Schema))
+        ):
             inner_map = self.resolve(self.schema.additionalProperties)
 
         if inner_map is not None:
@@ -224,7 +289,11 @@ class NamedSchema:
 
         if self.schema.type == DataType.OBJECT:
             if self.schema.properties:
-                result = NamedStructureType(definition_dependencies=[], properties={}, definitions=self.definitions)
+                result = NamedStructureType(
+                    definition_dependencies=[],
+                    properties={},
+                    definitions=self.definitions,
+                )
                 for key, value in self.schema.properties.items():
                     if not key.isidentifier():
                         return _struct
@@ -237,13 +306,30 @@ class NamedSchema:
                     if isinstance(value, Reference):
                         named.name = value.ref.split("/")[-1]
                     else:
-                        named.name =f"{self.name}_{key}"
+                        named.name = f"{self.name}_{key}"
                         result.definition_dependencies.append(key)
                 return result
             return _struct
 
-        if self.schema.type in (DataType.STRING, DataType.NUMBER, DataType.INTEGER, DataType.BOOLEAN, DataType.NUMBER):
+        if self.schema.type in (
+            DataType.STRING,
+            DataType.NUMBER,
+            DataType.INTEGER,
+            DataType.BOOLEAN,
+            DataType.NUMBER,
+        ):
             return ScalarType(self.schema.type, self.schema.schema_format)
+        if self.schema.const is not None:
+            if isinstance(self.schema.const, int):
+                return ScalarType(
+                    DataType.INTEGER,
+                    extension=f"(options.v1.const).int = {self.schema.const!r}",
+                )
+            if isinstance(self.schema.const, str):
+                return ScalarType(
+                    DataType.STRING,
+                    extension=f"(options.v1.const).string = {self.schema.const!r}",
+                )
 
         return _any
 
@@ -255,40 +341,60 @@ class TopLevelSchema:
 
     @property
     def schemas(self) -> "Iterator[NamedSchema]":
-        definitions = {k: Schema.model_validate(v) for k, v in self.schema.__pydantic_extra__.get('definitions', {}).items()}
+        definitions = {
+            k: Schema.model_validate(v)
+            for k, v in self.schema.__pydantic_extra__.get("definitions", {}).items()
+        }
         yield NamedSchema(self.schema.title, self.schema, definitions)
 
         for k, v in definitions.items():
             yield NamedSchema(k, Schema.model_validate(v), definitions)
 
+
 @register_test
 def an(obj: Any, kind: Any):
     return isinstance(obj, kind)
+
 
 @register_global
 def raise_helper(*parts: Any):
     raise RuntimeError(" ".join(str(p) for p in parts))
 
+
 @register_filter
 def as_comment(value: str | None) -> str:
-    if value is None: return ''
+    if value is None:
+        return ""
     return "/*\n" + value.strip() + "\n*/"
+
+
+@register_filter
+def as_pascal_case(value: str) -> str:
+    return value.title().replace("_", "")
+
+
+@register_filter
+def as_snake_case(value: str) -> str:
+    return re.sub("(.)([A-Z][a-z]+)", r"\1_\2", value).lower()
+
 
 def main():
     src_root = os.path.abspath(os.path.join(here, ".."))
-    print(f"Searching for protos in {src_root}...")
-    for src in glob.glob(f"{src_root}/**/*.schema.json"):
-        dst = src.replace(".schema.json", ".proto")
-        with open(src, "r") as source_schema, open(dst, "w") as output_proto:
-            print(f"Processing {src}")
-            d = json.load(source_schema)
-            d = cleaneup_jsonschema(d)
+    src = sys.argv[1]
+    assert os.path.abspath(src).startswith(src_root)
+    assert src.endswith(".schema.json")
+    dst = src.replace(".schema.json", ".proto")
+    with open(src, "r") as source_schema, open(dst, "w") as output_proto:
+        print(f"Processing {src}")
+        d = json.load(source_schema)
+        d = cleaneup_jsonschema(d)
 
-            schema = TopLevelSchema(
-                "sentry_protos." + os.path.relpath(src, src_root).replace("/", ".").removesuffix(".schema.json"),
-                                    Schema.model_validate(d))
+        schema = TopLevelSchema(
+            os.path.dirname(os.path.relpath(src, src_root)).replace("/", "."),
+            Schema.model_validate(d),
+        )
 
-            print(f"Writing {dst}...")
-            output_proto.write(
-                jinja_env.get_template("js2.proto.jinja").render(top_level=schema)
-            )
+        print(f"Writing {dst}...")
+        output_proto.write(
+            jinja_env.get_template("js2.proto.jinja").render(top_level=schema)
+        )

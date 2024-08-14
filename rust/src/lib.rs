83e9e34efc7ada00d5d296b5c2a5e857bf4e85f7
Mark Story <mark@mark-story.com>
2024-08-14 17:44:03 -0400

    Revert changes to rust/lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,18 +1,1 @@
-#[path = ""]
-pub mod events {
-    #[path = "sentry_protos.kafka.events.v1.rs"]
-    pub mod v1;
-}
-
-#[path = ""]
-pub mod options {
-    #[path = "sentry_protos.options.v1.rs"]
-    pub mod v1;
-}
-
-#[path = ""]
-pub mod relay {
-    #[path = "sentry_protos.relay.v1.rs"]
-    pub mod v1;
-}
-
+/// This file is replaced with generated content on build.

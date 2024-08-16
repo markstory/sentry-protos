5b9c41dcd327c0f24b10b61686c16fc2a7a8bcc7
Mark Story <mark@mark-story.com>
2024-08-16 15:53:52 -0400

    Take a different approach with generating rust code

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -11,11 +11,6 @@ NEW_VERSION="$2"
 # Update VERSION file
 echo "$NEW_VERSION" > VERSION
 
-# Build rust code
-# TODO(mark): This isn't great but I don't see another place I can hook into craft release
-# cycle to generate the necessary code for rust.
-make build-rust
-
 # Update rust/Cargo.toml
 perl -pi -e "s/^version = \".*?\"/version = \"$NEW_VERSION\"/" rust/Cargo.toml
 

8d6ad00b62ad3dafbe52835b60bbc1f4048dba43
Mark Story <mark@mark-story.com>
2024-08-23 15:43:47 -0400

    Add postrelease script for cleaning up rust packages

---
+++ scripts/post-release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+# Configure git
+git config user.email bot@getsentry.com
+git config user.name getsentry-bot
+
+# Cleanup repository after releases are created
+git rm -r rust/src
+
+# An empty lib.sh is required for cargo to run the generator.
+touch rust/src/lib.sh
+git add rust/src/lib.sh
+
+git commit -m "Cleanup generated rust code"
+git pull --rebase && git push
